c83cc8520d54eae651b8c29fa370ae4280f52db7
Jeremy Banks <jeremybankstech@gmail.com>
2025-12-19 01:25:19 -0800

    New method (#1)
    * initial working flat rework
    
    * done

--- README.md
+++ README.md
@@ -4,8 +4,15 @@ I built this script to quickly and easily take advantage of the volatility in cr
 
 Specifically, I believe BTC will crash again as it did in Dec 2022, and I also believe it will reach a new all-time high relatively shortly thereafter. Everything I know about finance and economics says the best strategy for investing in a volatile asset is to use dollar cost averaging. There are two ways to achieve this: buy a little every day like Microstrategy and El Salvador, or set up limit buy orders in specific price steps to catch every valuation. Since I am not working with billions like MSTR and El Salvator, I have to use the second method.
 
-```
-python3 coinbase-dca.py BTC-USD 80000 1000 250 80
-```
+This script features two modes for investment, flat and aggressive. Flat is just that: spending the same at each step. Aggressive is based on a geometric progression: spending more at lower steps.
 
-The above example creates limit buy orders in BTC-USD, beginning at the $80,000 level and ending at the $1,000 level, and placing orders for $80 at each $250 step (80000, 79750, 79500, etc).
+```
+# create orders BTC-USD starting at $80,000 and ending at $22,000, at every $250 step, investing $1000
+python3 coinbase-dca.py BTC-USD flat 80000 22000 250 1000
+
+# create orders LTC-USD starting at $80,000 and ending at $22,000, at every $250 step, investing $1000
+python3 coinbase-dca.py LTC-USD flat 70 20 5 1000
+
+# create orders starting at $80,000 and ending at $22,000, at every $250 step, investing $20000
+python3 coinbase-dca.py BTC-USD aggr 80000 22000 250 20000
+```
--- coinbase-dca.py
+++ coinbase-dca.py
@@ -14,27 +14,82 @@ API_KEY = ""
 API_SECRET = """"""
 
 def main():
+    product_id = sys.argv[1]
+    mode = sys.argv[2]
+    price_high = float(sys.argv[3])
+    price_low = float(sys.argv[4])
+    price_step = float(sys.argv[5])
+    total_usd = float(sys.argv[6])
 
-    # for testing
+    price_range = price_high - price_low
+    number_of_orders = round(price_range / price_step)
+
+    price = price_high
+    usd_per_order = 0
+
+    round_to = 8
+
+    # # for testing
     # client = RESTClient(api_key=API_KEY, api_secret=API_SECRET)
     # print(client.get_accounts())
 
-    if len(sys.argv) != 6:
-        print("usage: python coinbase-dca.py <product_id> <price_high> <price_low> <price_step> <usd_per_order>")
+    if len(sys.argv) != 7:
+        print("usage: python coinbase-dca.py <product_id> <mode> <price_high> <price_low> <price_step> <total_usd>")
         sys.exit(1)
 
-    product_id = sys.argv[1]
-    price_high = float(sys.argv[2])
-    price_low = float(sys.argv[3])
-    price_step = float(sys.argv[4])
-    usd_per_order = float(sys.argv[5])
+    if mode != "flat" and mode != "aggr":
+        print("mode must be 'flat' or 'aggr'")
+        sys.exit(1)
 
     client = RESTClient(api_key=API_KEY, api_secret=API_SECRET)
 
-    price = price_high
     while price >= price_low:
-        base_size = round(usd_per_order / price, 8)
-        print(f"placing limit buy: ${usd_per_order} (~{base_size} ${product_id}) @ ${price}")
+        if mode == "flat":
+            # print("flat")
+            usd_per_order = round(total_usd / number_of_orders, 2)
+
+            base_size = round(usd_per_order / price, round_to)
+            base_size = f"{base_size:.{round_to}f}"
+            
+            print(f"placing limit buy: ${usd_per_order} (~{base_size} ${product_id}) @ ${price}")
+        elif mode == "aggr":
+            # print("aggr")
+            # print(price)
+
+            price_1x = price_high
+            price_2x = price_high - price_range * 0.125   # 12.5% drop from high
+            price_4x = price_high - price_range * 0.25    # 25% drop from high
+            price_8x = price_high - price_range * 0.5     # 50% drop from high
+
+            # print(price_1x)
+            # print(price_2x)
+            # print(price_4x)
+            # print(price_8x)
+
+            if price <= price_8x:
+                # print("8x")
+                # print(round((total_usd * 0.50) / number_of_orders, 2))
+                usd_per_order = round((total_usd * 0.50) / number_of_orders, 2)
+            elif price <= price_4x:
+                # print("4x")
+                # print(round((total_usd * 0.50 * 0.50) / number_of_orders, 2))
+                usd_per_order = round((total_usd * 0.50 * 0.50) / number_of_orders, 2)
+            elif price <= price_2x:
+                # print("2x")
+                # print(round((total_usd * 0.50 * 0.50 * 0.50) / number_of_orders, 2))
+                usd_per_order = round((total_usd * 0.50 * 0.50 * 0.50) / number_of_orders, 2)
+            elif price > price_2x:
+                # print("1x")
+                # print(round((total_usd * 0.50 * 0.50 * 0.50 * 0.50) / number_of_orders, 2))
+                usd_per_order = round((total_usd * 0.50 * 0.50 * 0.50 * 0.50) / number_of_orders, 2)
+            else:
+                print("ERROR")
+                sys.exit(1)
+
+            base_size = round(usd_per_order / price, round_to)
+            base_size = f"{base_size:.{round_to}f}"
+
+            print(f"placing limit buy: ${usd_per_order} (~{base_size} ${product_id}) @ ${price}")
 
         client.create_order(
             client_order_id=str(uuid.uuid4()),
@@ -49,6 +104,7 @@ def main():
         )
 
         price -= price_step
+        price = round(price, 2)
         time.sleep(0.2) # rate limit
 
 if __name__ == "__main__":
